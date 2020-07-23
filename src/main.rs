f245c35a21f2eaf2538b187ebb4167e9c492b6b0
Jediah Katz <jediahkatz@gmail.com>
2020-07-22 20:34:46 -0400

    Image file with progress indicator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+
+    let img_width: i32 = 256;
+    let img_height: i32 = 256;
+
+    println!("P3\n{} {}\n255", img_width, img_height);
+
+    for j in (0..img_height).rev() {
+        eprintln!("\rScanlines remaining: {}", j);
+        for i in 0..img_width {
+            let r: f64 = f64::from(i)/f64::from(img_height-1);
+            let g: f64 = f64::from(j)/f64::from(img_width-1);
+            let b: f64 = 0.25;
+
+            let ir: i32 = (255.999 * r) as i32;
+            let ig: i32 = (255.999 * g) as i32;
+            let ib: i32 = (255.999 * b) as i32;
+
+            println!("{} {} {}", ir, ib, ig);
+        }
+    }
+
+    eprintln!("Done")
 }
